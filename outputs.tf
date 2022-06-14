e5f635a504bc85961888ac6378197ba8a5155d42
jvalderrama <joedval@gmail.com>
2022-06-14 09:40:29 +0200

    Set description for output variables

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "cdn_url" {
+  description = "The internet-facing endpoint created for the fastly service created"
   value = "https://${var.domain}"
 }
 
 output "service_id" {
+  description = "The fastly service identifier created"
   value = fastly_service_vcl.service.id
 }
